927801d1c5a22022fd559fca04b21de4830cd52e
dnmore <morettidn@gmail.com>
2025-11-06 19:01:20 +0100

    add german language support

--- app/i18n/index.ts
+++ app/i18n/index.ts
@@ -1,9 +1,10 @@
 import { en } from "./messages/en";
 import { it } from './messages/it';
 import { fr} from './messages/fr';
-export type Locale = 'en' | 'it' | 'fr';
+import { de} from './messages/de';
+export type Locale = 'en' | 'it' | 'fr' | 'de';
 
-export const SUPPORTED_LOCALES: Locale[] = ['en','it', 'fr'];
+export const SUPPORTED_LOCALES: Locale[] = ['en','it', 'fr', 'de'];
 
 export const DEFAULT_LOCALE: Locale = 'en';
 
@@ -11,4 +12,5 @@ export const messagesMap: Record<Locale, Record<string, string>> = {
   en: en as Record<string,string>,
   it: it as Record<string,string>,
   fr: fr as Record<string,string>,
+  de: de as Record<string,string>,
 };
---
+++ app/i18n/messages/de.ts
@@ -0,0 +1,36 @@
+export const de = {
+  'app.create': 'neue Aufgabe',
+  'app.rules': 'Regeln',
+  'app.modalTitle-create':  'Aufgabe hinzufügen',
+  'app.modalDesc-create': ' Fügen Sie Aufgabendetails hinzu.',
+  'app.modalTitle-update':  'Aufgabe bearbeiten',
+  'app.modalDesc-update': 'Bearbeiten Sie die gewünschten Felder.',
+  'app.descLabel': 'Aufgabe',
+  'app.priorityLabel': 'Priorität',
+  'app.optionLow': 'Niedrige Priorität',
+  'app.optionMedium': 'Mittlere Priorität',
+  'app.optionHigh': 'Hohe Priorität',
+  'app.dueDateLabel': 'Fälligkeitsdatum',
+  'app.save': 'Speichern',
+  'app.rules-title': 'Regeln',
+  'app.rules-description': 'Hier sind die Spielregeln:',
+  'app.rules-1': 'Aufgaben zu erledigen, um Punkte zu sammeln',
+  'app.rules-2': 'Erreichen Sie verschiedene Stufen basierend auf Ihren Punkten: Novice, Pro, Guru.',
+  'app.rules-3': 'Das Erledigen von Aufgaben mit höherer Priorität führt zu mehr Punkten.',
+  'app.rules-4': 'Klicken Sie auf TO DO, um Aufgaben als ERLEDIGT zu markieren und Punkte zu sammeln.',
+  'app.points': 'Punkte: {points}',
+  'app.priority-filter': 'Aufgaben nach Priorität filtern',
+  'app.optionAll': 'Alle Aufgaben',
+   'app.edit': 'Bearbeiten',
+   'app.delete': 'Löschen',
+   'app.todo': 'To Do',
+  'app.done': 'Erledigt',
+   'app.update': 'Aktualisieren',
+   'app.emptyState': 'Keine Aufgaben',
+  
+
+
+  
+} as const;
+
+export type EnMessages = typeof de;
